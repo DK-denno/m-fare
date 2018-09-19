11ec468791cfa0c63fd63cf1cb2b50e9fe21b245
EugeneZnm <eugenenzioki@gmail.com>
2018-09-19 11:10:36 +0300

    admin authentication template created

--- app/models.py
+++ app/models.py
@@ -1,32 +1,30 @@
 from . import db
 from flask_login import UserMixin, login_user
-from werkzeug.security import generate_password_hash,check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class Roles(UserMixin,db.Model):
+
+class Roles(UserMixin, db.Model):
     __tablename__ = 'roles'
-
-    id = db.Column(db.Integer,primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     role = db.Column(db.String(255))
-    user = db.relationship('User',backref='users',lazy="dynamic")
+    user = db.relationship('User', backref='users', lazy="dynamic")
 
 
-class User(UserMixin,db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
-
-    id = db.Column(db.Integer,primary_key=True)
-    email = db.Column(db.String(255),unique=True,index = True)
-    username = db.Column(db.String(255),index = True)
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True, index=True)
+    username = db.Column(db.String(255), index=True)
     password_secure = db.Column(db.String(255))
     phone_number = db.Column(db.Integer)
-    role = db.Column(db.String(255),db.ForeignKey('roles.role'))
-
+    role = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     @classmethod
     def save_user(self):
         db.session.add(self)
         db.session.commit()
-        
-    def delete_user(cls,id):
+
+    def delete_user(cls, id):
         db.session.delete(id)
         db.session.commit()
 
@@ -35,48 +33,46 @@ class User(UserMixin,db.Model):
         raise AttributeError('YOU CANNOT ACCESS THIS DETAILS')
 
     @password.setter
-    def password(self,password):
+    def password(self, password):
         self.password_secure = generate_password_hash(password)
 
-    def verify_password(self,password):
-        return check_password_hash(self.password_secure,password)
+    def verify_password(self, password):
+        return check_password_hash(self.password_secure, password)
 
 
-class Sacco(UserMixin,db.Model):
+class Sacco(UserMixin, db.Model):
     __tablename__ = 'sacco'
-    id = db.Column(db.Integer,primary_key=True)
-    email = db.Column(db.String(255),unique=True,index = True)
-    sacconame = db.Column(db.String(255),index = True)
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True, index=True)
+    sacconame = db.Column(db.String(255), index=True)
     password_secure = db.Column(db.String(255))
     phone_number = db.Column(db.Integer)
     route = db.Column(db.String(255))
-    fares = db.relationship('Fares',backref='fare',lazy="dynamic")
+    fares = db.relationship('Fares', backref='fare', lazy="dynamic")
 
     @classmethod
     def save_sacco(self):
         db.session.add(self)
         db.session.commit()
 
-    def delete_sacco(cls,id):
+    def delete_sacco(cls, id):
         db.session.delete(id)
         db.session.commit()
-
 
     @property
     def password(self):
         raise AttributeError('YOU CANNOT ACCESS THIS DETAILS')
 
     @password.setter
-    def password(self,password):
+    def password(self, password):
         self.password_secure = generate_password_hash(password)
 
-    def verify_password(self,password):
-        return check_password_hash(self.password_secure,password)
-    
+    def verify_password(self, password):
+        return check_password_hash(self.password_secure, password)
+
+
 class Fares(db.Model):
-    __tablename__='fares'
-    id = db.Column(db.Integer,primary_key=True)
+    __tablename__ = 'fares'
+    id = db.Column(db.Integer, primary_key=True)
     fare = db.Column(db.Integer)
     sacco_id = db.Column(db.ForeignKey('sacco.id'))
-
-        
