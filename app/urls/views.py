e21a080efc550859c9b2c91163059e4096520651
dk-denno <dennisveer27@gmail.com>
2018-09-20 18:31:45 +0300

    df

--- app/urls/views.py
+++ app/urls/views.py
@@ -24,7 +24,7 @@ def africa_talking():
         # Specify the numbers that you want to send to in a comma-separated list
         # Please ensure you include the country code (+254 for Kenya)
 
-        to = '+254725328016'
+        to = users
         # And of course we want our recipients to know what we really do
 
         message = saccotext.text.data
